bdd471158ce275aca27ff4f27cdfccacfce21243
ollieshmollie <oliverduncan@icloud.com>
2016-12-04 12:30:53 -0600

    Implement saving and deleting purchases with fetchedObjects controller

--- PurchaseTableViewController.swift
+++ PurchaseTableViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 class PurchaseTableViewController: UITableViewController {
 
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var amountField: UITextField!
     
+    let context = DataController.sharedInstance.persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        toggleSaveButton()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,4 +30,27 @@ class PurchaseTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
 
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        let name = nameField.text!
+        let amount = amountField.text!
+        let date = datePicker.date
+        let purchase = Purchase(context: context)
+        purchase.name = name
+        purchase.amount = Double(amount)!
+        purchase.date = date.timeIntervalSinceReferenceDate
+        DataController.sharedInstance.saveContext()
+    }
+    
+    @IBAction func validate(_ sender: UITextField) {
+        toggleSaveButton()
+    }
+    
+    func toggleSaveButton() {
+        saveButton.isEnabled = (!nameField.text!.isEmpty && !amountField.text!.isEmpty)
+    }
+    
 }
---
+++ iSpend/PurchasesViewController.swift
@@ -0,0 +1,90 @@
+//
+//  PurchasesViewController.swift
+//  iSpend
+//
+//  Created by Oliver Duncan on 12/3/16.
+//  Copyright © 2016 BondTwins. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class PurchasesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let context: NSManagedObjectContext = DataController.sharedInstance.persistentContainer.viewContext
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Purchase> = {
+        let request: NSFetchRequest<Purchase> = Purchase.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        return NSFetchedResultsController<Purchase>.init(fetchRequest: request, managedObjectContext: self.context, sectionNameKeyPath: nil, cacheName: nil)
+    }()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("An error occurred")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Table view datasource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let purchases = fetchedResultsController.fetchedObjects else { return 0 }
+        return purchases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PurchasesTableCell")!
+        let purchase = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = purchase.name
+        cell.detailTextLabel?.text = String(purchase.amount)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+            case .delete:
+                context.delete(fetchedResultsController.object(at: indexPath))
+                DataController.sharedInstance.saveContext()
+            default: return
+        }
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
+
+extension PurchasesViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+            case .delete:
+                tableView.deleteRows(at: [indexPath!], with: .automatic)
+            default: return
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.tableView.endUpdates()
+    }
+}
